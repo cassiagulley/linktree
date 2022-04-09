e4535701b0d02f0c5be6d85ff9a6040b1ba68460
Cassia Gulley <cassia.gulley@codebots.com>
2022-04-09 17:11:45 +1000

    Added basic link service

---
+++ LinktreeTakeHome/Services/LinkService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using LinktreeTakeHome.Models;
+
+namespace LinktreeTakeHome.Services
+{
+	// An in-memory link data service class
+	public static class LinkService
+	{
+		static List<Link> Links { get; }
+
+
+		public static  LinkService()
+		{
+			// TODO: Create/add new links here for preliminary testing
+		}
+
+        // TODO: Add helper functions to match poject specifications
+
+        public static List<Link> GetAll() => Links;
+
+        public static Link? Get(int id) => Links.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Link link)
+        {
+            // Do links need to generate new Ids or are they given?
+            Links.Add(link);
+        }
+
+        public static void Delete(int id)
+        {
+            var link = Get(id);
+            if (link is null)
+                return;
+
+            Links.Remove(link);
+        }
+
+        public static void Update(Link link)
+        {
+            var index = Links.FindIndex(p => p.Id == link.Id);
+            if (index == -1)
+                return;
+
+            Links[index] = link;
+        }
+
+    }
+
+
+}
+
